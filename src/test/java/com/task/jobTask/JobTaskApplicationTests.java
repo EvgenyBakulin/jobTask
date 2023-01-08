48bb60ba0db6ca4a4d574b8d4ce180245045104f
Евгений <ugend84@yandex.ru>
2023-01-08 12:30:54 +0300

    Initial commit

---
+++ src/test/java/com/task/jobTask/JobTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.jobTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
